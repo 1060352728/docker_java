dccd46998d1aa42d35231b4d1ec13ff2c30b3f2d
likui <1060352728@qq.com>
2024-11-11 23:08:19 +0800

    docker测试java部署

---
+++ src/main/java/com/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * 功能描述:  权限测试
+ *
+ * @date: 2019/1/7 21:19
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/getDate")
+    public String getDate() {
+        return "这是一个Docker前后台部署测试";
+    }
+}
